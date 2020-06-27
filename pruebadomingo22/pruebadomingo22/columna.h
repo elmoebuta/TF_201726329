4c72a29fa8abf41bc2573dcfdd9f2bf35be05993
Jose Carlos Aldana Garcia <u201726329@upc.edu.pe>
2020-06-27 01:14:31 -0500

    version 2.0

--- pruebadomingo22/pruebadomingo22/columna.h
+++ pruebadomingo22/pruebadomingo22/columna.h
@@ -3,26 +3,52 @@
 #include<vector>
 #include<string>
 using namespace std;
-template<class T>
+
 class ccolumna
 {
 public:
 	ccolumna()
 	{
-		columna = vector<T>();
+		nombre = "prueba";
+		tipo = 1;
+		vector<int> entero=new vector<int>;
+		vector<string> palabra=new vector <string>;
+		
+
 	
 	};
+	ccolumna(string nombre,int tipo)
+	{
+		this->nombre = nombre;
+		this->tipo = tipo;
+		vector<int> entero = new vector<int>;
+		vector<string> palabra= new vector <string>;
+
+	};
+	void agregarentero(int dato) {
+		entero.push_back(dato);
+	}
+	void agregarpalabra(string dato) {
+		palabra.push_back(dato);
+	}
+	string getpalabra(int n)
+	{
+		return palabra[n];
+	}
+	int getentero(int n)
+	{
+		return entero[n];
+	}
 	~ccolumna() {};
-	void agregar(T dato)
-	{
-		columna.push_back(dato);
-	}
-	int longitud()
-	{
-		return columna.size();
-	}
+	agregar
+	
 
 public:
-	vector<T> columna;
+	
+	string nombre;
+	int tipo;
+	vector<int> entero;
+	vector<string> palabra;
+	
 };
 
